3b013386617dc7e9a9f048a550daef6918f83030
star <star@stars-Mac.local>
2017-05-19 14:04:54 -0700

    Initial Commit

---
+++ iosreviewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosreviewApp
+//
+//  Created by star on 5/19/17.
+//  Copyright © 2017 star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iosreviewApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iosreviewApp
+//
+//  Created by star on 5/19/17.
+//  Copyright © 2017 star. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
